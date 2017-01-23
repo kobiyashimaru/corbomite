2c3a9cead44eba4757497beb6c80bd856a21aaed
coreysf <corey.j.roy@gmail.com>
2017-01-22 21:39:08 -0800

    Add initial state and create event handler to set state

--- public/app.jsx
+++ public/app.jsx
@@ -1,21 +1,46 @@
 var Greeter = React.createClass({
   getDefaultProps:function() {
     return {
-      name: "React"
+      name: "React",
+      message: "This is from a component"
     };
   },
+  getInitialState: function() {
+    return {
+      name: this.props.name
+    };
+  },
+  onButtonClick: function(e) {
+    e.preventDefault();
+    var name = this.refs.name.value;
+    this.refs.name.value = "";
+
+    this.setState({
+      name: name
+    });
+  },
   render: function() {
-    var name = this.props.name;
+    var name = this.state.name;
+    var message = this.props.message;
+
     return (
       <div>
         <h1>Hello {name}!</h1>
-        <p>This is from a component</p>
+        <p>{message}</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name" />
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
 });
 
+var firstName = "Corey";
+var message = "Having fun with props!"
+
 ReactDOM.render(
-  <Greeter name="Corey"/>,
+  <Greeter name={firstName} message={message} />,
   document.getElementById("app")
 );
